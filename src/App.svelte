ba671e539099de6b22fdd30c577da145fd3acf84
leilani <lherna05@gmail.com>
2022-08-30 20:04:58 -0700

    updated to fix unknown prop issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,57 @@
 <script>
+	import {useForm, Hint, HintGroup, validators, minLength,email,required} from "svelte-use-form";
+	const form = useForm();
+
 	export let name;
+	export let questions = 'What are your goals for today?';
+	export let counter;
+	let color = 'orange';
+	const currentTasks = [];
+
+	$: {
+		console.log('CurrentTasks: ', currentTasks);
+		console.log('Counter: ', counter)
+	}
+
+	const handleClick = () => {
+		const questionArr = ['What will you accomplish today?', 
+		'What will you figure out today?', 
+		'What needs to get done today?', 
+		'What are your goals today?'];
+		console.log('counter is ', counter)
+		if (questionArr[counter]) {
+			questions = questionArr[counter];
+			counter++
+		} else {
+			counter = 0; 
+			questions = questionArr[counter];
+		}
+	}
+	
+	const handleInput = (e) => {
+		const newInput = e.target.value; 
+		currentTasks.push(newInput);
+		console.log('Current tasks are now: ', currentTasks)
+
+	}
+
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p style="color: {color}">{questions}</p>
+	<button on:click={handleClick} value={questions}> Ask me another!</button>
+
+	<section>
+		<h2>Project Tasks</h2>
+		<form on:submit={handleInput} >
+			<input type='text' placeholder='Enter new task here'>	
+		</form>
+		<p>
+			{currentTasks}
+		</p>
+	</section>
 </main>
 
 <style>
